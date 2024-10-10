f140ff3fc593efa29ffbf35dee6e1bca821047b6
Gilbert Eaton <gilberteaton@gmail.com>
2024-10-10 16:46:16 +1000

    fix - running as a service

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
-# run the uvicorn server
-python3 web/server.py 
+#ENV=/home/trial/.virtualenv/dev
+
+#set -e
+#source $ENV/bin/activate 
+
+
+if [[ "$VIRTUAL_ENV" != "" ]]; then 
+    echo "You are in working virtualenv $VIRTUAL_ENV";
+    #check requirements file exists
+    cd /home/trial/git/BackgroundSpeedTest/
+    FILE=web/server.py
+    if [[ -f "$FILE" ]]; then
+	#run the server
+	echo "";
+	echo "Running server in the background...";
+	python $FILE;
+    else 
+	echo "$FILE does not exist.";
+	echo "Exiting...";
+    fi
+
+else
+    echo "You are not working in a virtual environment...";
+    echo "Exiting.";
+fi
 
