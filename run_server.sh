8b8b6ac2c3a704d5f7f4bd970c50cf8ff2a707b4
gilbert eaton <gilberteaton@gmail.com>
2024-10-18 08:52:29 +1000

    feat - updated bash script for user input

--- run_server.sh
+++ run_server.sh
@@ -1,28 +1,36 @@
 #!/usr/bin/env bash
 
-#ENV=/home/trial/.virtualenv/dev
+#ENV=/path/to/.virtualenv/your_env
 
 #set -e
 #source $ENV/bin/activate 
 
+#check requirements file exists
+cd /home/trial/git/BackgroundSpeedTest/
+FILE=web/server.py
 
 if [[ "$VIRTUAL_ENV" != "" ]]; then 
     echo "You are in working virtualenv $VIRTUAL_ENV";
-    #check requirements file exists
-    cd /home/trial/git/BackgroundSpeedTest/
-    FILE=web/server.py
     if [[ -f "$FILE" ]]; then
-	#run the server
-	echo "";
-	echo "Running server in the background...";
-	python $FILE;
+	    #run the server
+	    echo "";
+	    echo "Running server in the background...";
+	    python $FILE;
     else 
 	echo "$FILE does not exist.";
-	echo "Exiting...";
+	    echo "Exiting...";
     fi
 
 else
     echo "You are not working in a virtual environment...";
-    echo "Exiting.";
+    printf "Do you want to run the server anyway? (y/n): ";
+    read answer;
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+	    echo "";
+	    echo "Running server in the background...";
+	    python $FILE;
+    else
+	    echo "Exiting...";
+    fi
 fi
 
